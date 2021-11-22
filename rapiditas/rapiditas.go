391bb60748588d2b86ddc59627481f0262732c5b
Christos Ploutarchou <cploutarchou@gmail.com>
2021-11-22 18:28:46 +0200

    update logs structure

--- rapiditas/rapiditas.go
+++ rapiditas/rapiditas.go
@@ -3,6 +3,9 @@ package rapiditas
 import (
 	"fmt"
 	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"strconv"
 )
 
 const (
@@ -10,9 +13,14 @@ const (
 )
 
 type Rapiditas struct {
-	AppName string
-	Debug   bool
-	Version string
+	AppName    string
+	Debug      bool
+	Version    string
+	ErrorLog   *log.Logger
+	InfoLog    *log.Logger
+	WarningLog *log.Logger
+	BuildLog   *log.Logger
+	RootPath   string
 }
 
 func (r *Rapiditas) New(rootPath string) error {
@@ -34,11 +42,19 @@ func (r *Rapiditas) New(rootPath string) error {
 	if err != nil {
 		return err
 	}
+	// implement Logger
+	infoLog, errorLog, warnLog, buildLog := r.startLogger()
+	r.InfoLog = infoLog
+	r.ErrorLog = errorLog
+	r.WarningLog = warnLog
+	r.BuildLog = buildLog
+
+	r.Debug, _ = strconv.ParseBool(os.Getenv("DEBUG"))
+	r.Version = version
 	return nil
 }
 
 func (r *Rapiditas) Init(p initPaths) error {
-	fmt.Print(version)
 	root := p.rootPath
 	for _, path := range p.foldersNames {
 		// create the directory if it doesn't exist'
@@ -56,4 +72,17 @@ func (r *Rapiditas) checkENV(path string) error {
 		return err
 	}
 	return nil
+}
+
+func (r *Rapiditas) startLogger() (*log.Logger, *log.Logger, *log.Logger, *log.Logger) {
+	var infoLog *log.Logger
+	var errorLog *log.Logger
+	var warnLog *log.Logger
+	var buildLog *log.Logger
+
+	warnLog = log.New(os.Stderr, "[ WARNING ] ", log.Ldate|log.Ltime|log.Lshortfile)
+	infoLog = log.New(os.Stderr, "[ INFO ] ", log.Ldate|log.Ltime|log.Lshortfile)
+	buildLog = log.New(os.Stderr, "[ BUILD ] ", log.Ldate|log.Ltime|log.Lshortfile)
+	errorLog = log.New(os.Stderr, "[ ERROR ] ", log.Ldate|log.Ltime|log.Lshortfile)
+	return infoLog, errorLog, warnLog, buildLog
 }
