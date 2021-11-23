1674d674999bbcbddac828f9a1e55bcf8cf70f65
Christos Ploutarchou <cploutarchou@gmail.com>
2021-11-23 20:50:01 +0200

    extend tests for page/template renderer

--- microGo/render/render_test.go
+++ microGo/render/render_test.go
@@ -18,12 +18,53 @@ func TestRenderPage(t *testing.T) {
 	testRenderer.RootPath = "./test"
 	err = testRenderer.Page(w, r, "home", nil, nil)
 	if err != nil {
-		t.Error("Unable to render page. ", err)
+		t.Error("Unable to render template. ", err)
 	}
 
+	err = testRenderer.Page(w, r, "not-exist", nil, nil)
+	if err == nil {
+		t.Error("Something went wrong. Unable to render a not existing template.", err)
+	}
 	testRenderer.Renderer = "jet"
 	err = testRenderer.Page(w, r, "home", nil, nil)
 	if err != nil {
-		t.Error("Unable to render page. ", err)
+		t.Error("Unable to render template. ", err)
+	}
+	err = testRenderer.Page(w, r, "not-exist", nil, nil)
+	if err == nil {
+		t.Error("Something went wrong. Unable to render a not existing  jet template.", err)
+	}
+	testRenderer.Renderer = ""
+	err = testRenderer.Page(w, r, "home", nil, nil)
+	if err == nil {
+		t.Error("No error while trying to render template with no valid engine. ", err)
+	}
+
+}
+
+func TestRenderGoPage(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("GET", "/url", nil)
+	if err != nil {
+		t.Error(err)
+	}
+	testRenderer.Renderer = "go"
+	testRenderer.RootPath = "./test"
+	err = testRenderer.Page(w, r, "home", nil, nil)
+	if err != nil {
+		t.Error("Unable to render Go template. ", err)
+	}
+}
+func TestRenderJetPage(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("GET", "/url", nil)
+	if err != nil {
+		t.Error(err)
+	}
+	testRenderer.Renderer = "jet"
+	testRenderer.RootPath = "./test"
+	err = testRenderer.Page(w, r, "home", nil, nil)
+	if err != nil {
+		t.Error("Unable to render Jet template. ", err)
 	}
 }
