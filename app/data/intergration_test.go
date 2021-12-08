1301aa98f5563bda636a926e020fc97164959608
Christos Ploutarchou <cploutarchou@gmail.com>
2021-12-08 20:00:52 +0200

    Creating tests for database tables in  docker image, and running some tests

--- app/data/intergration_test.go
+++ app/data/intergration_test.go
@@ -1,13 +1,21 @@
+//go:build integration
+
+// run tests with this command: go test . --tags integration --count=1
+
 package data
 
 import (
 	"database/sql"
 	"fmt"
-	"github.com/ory/dockertest/v3"
-	"github.com/ory/dockertest/v3/docker"
 	"log"
 	"os"
 	"testing"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
 )
 
 var (
@@ -16,8 +24,9 @@ var (
 	password = "secretpassword"
 	dbName   = "microGo_test"
 	port     = "5439"
-	dsn      = "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable connect_timeout=5"
+	dsn      = "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable timezone=Europe/Athens connect_timeout=5"
 )
+
 var dummyUSER = User{
 	FirstName: "Christos",
 	LastName:  "Ploutarchou",
@@ -27,46 +36,205 @@ var dummyUSER = User{
 }
 var models Models
 var testDB *sql.DB
-var resource = *dockertest.Resource
-var pool = *dockertest.Pool
+var resource *dockertest.Resource
+var pool *dockertest.Pool
 
 func TestMain(m *testing.M) {
-	_ = os.Setenv("DATABASE_TYPE", "postgres")
+	os.Setenv("DATABASE_TYPE", "postgres")
+
 	p, err := dockertest.NewPool("")
 	if err != nil {
-		log.Fatal("Unable to connect with docker. ")
+		log.Fatalf("Something went wrong, unable not connect to docker: %s", err)
 	}
+
 	pool = p
 
 	opts := dockertest.RunOptions{
 		Repository: "postgres",
-		Tag:        "14-bullseye",
+		Tag:        "13.4",
 		Env: []string{
 			"POSTGRES_USER=" + user,
 			"POSTGRES_PASSWORD=" + password,
 			"POSTGRES_DB=" + dbName,
-		}, ExposedPorts: []string{"5432"},
+		},
+		ExposedPorts: []string{"5432"},
 		PortBindings: map[docker.Port][]docker.PortBinding{
 			"5432": {
 				{HostIP: "0.0.0.0", HostPort: port},
 			},
 		},
 	}
+
 	resource, err = pool.RunWithOptions(&opts)
 	if err != nil {
 		_ = pool.Purge(resource)
-		log.Fatalf("Could not start docker resource: %s", err)
+		log.Fatalf("Something went wrong, could not start resource: %s", err)
 	}
 
 	if err := pool.Retry(func() error {
 		var err error
 		testDB, err = sql.Open("pgx", fmt.Sprintf(dsn, host, port, user, password, dbName))
 		if err != nil {
+			log.Println("Error:", err)
 			return err
 		}
 		return testDB.Ping()
 	}); err != nil {
 		_ = pool.Purge(resource)
-		log.Fatalf("Unable connect to docker: %s", err)
+		log.Fatalf("Something went wrong, could not connect to docker: %s", err)
+	}
+
+	err = createTables(testDB)
+	if err != nil {
+		log.Fatalf("Something went wrong. Unable to create tables: %s", err)
+	}
+
+	models = New(testDB)
+
+	code := m.Run()
+
+	if err := pool.Purge(resource); err != nil {
+		log.Fatalf("Something went wrong , could not purge resource: %s", err)
+	}
+
+	os.Exit(code)
+}
+
+func createTables(db *sql.DB) error {
+	stmt := `
+	CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+drop table if exists users cascade;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    first_name character varying(255) NOT NULL,
+    last_name character varying(255) NOT NULL,
+    user_active integer NOT NULL DEFAULT 0,
+    email character varying(255) NOT NULL UNIQUE,
+    password character varying(60) NOT NULL,
+    created_at timestamp without time zone NOT NULL DEFAULT now(),
+    updated_at timestamp without time zone NOT NULL DEFAULT now()
+);
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+drop table if exists remember_tokens;
+
+CREATE TABLE remember_tokens (
+    id SERIAL PRIMARY KEY,
+    user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    remember_token character varying(100) NOT NULL,
+    created_at timestamp without time zone NOT NULL DEFAULT now(),
+    updated_at timestamp without time zone NOT NULL DEFAULT now()
+);
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON remember_tokens
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+drop table if exists tokens;
+
+CREATE TABLE tokens (
+    id SERIAL PRIMARY KEY,
+    user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    first_name character varying(255) NOT NULL,
+    email character varying(255) NOT NULL,
+    token character varying(255) NOT NULL,
+    token_hash bytea NOT NULL,
+    created_at timestamp without time zone NOT NULL DEFAULT now(),
+    updated_at timestamp without time zone NOT NULL DEFAULT now(),
+    expiry timestamp without time zone NOT NULL
+);
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON tokens
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+	`
+
+	_, err := db.Exec(stmt)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func TestUser_Table(t *testing.T) {
+	s := models.Users.Table()
+	if s != "users" {
+		t.Error("Something went wrong, No valid table name returned: ", s)
+	}
+}
+
+func TestUser_Insert(t *testing.T) {
+	id, err := models.Users.Insert(dummyUSER)
+	if err != nil {
+		t.Error("Something went wrong, failed to insert user: ", err)
+	}
+
+	if id == 0 {
+		t.Error("Something went wrong,  0 returned as id after insert")
+	}
+}
+
+func TestUser_Get(t *testing.T) {
+	u, err := models.Users.GetByID(1)
+	if err != nil {
+		t.Error("Something went wrong, unable to get user: ", err)
+	}
+
+	if u.ID == 0 {
+		t.Error("Something went wrong, returned user id  0 : ", err)
+	}
+}
+
+func TestUser_GetAll(t *testing.T) {
+	_, err := models.Users.GetAll()
+	if err != nil {
+		t.Error("Something went wrong, unable to get user: ", err)
+	}
+}
+
+func TestUser_GetByEmail(t *testing.T) {
+	u, err := models.Users.GetByEmail("cploutarchou@gmail.com")
+	if err != nil {
+		t.Error("Something went wrong, unable to get user: ", err)
+	}
+
+	if u.ID == 0 {
+		t.Error("Something went wrong, returned user id  0: ", err)
+	}
+}
+
+func TestUser_Update(t *testing.T) {
+	u, err := models.Users.GetByID(1)
+	if err != nil {
+		t.Error("Something went wrong, unable to get user: ", err)
+	}
+
+	u.LastName = "Kiriakou"
+	err = u.Update(*u)
+	if err != nil {
+		t.Error("Something went wrong, unable to update user: ", err)
+	}
+
+	u, err = models.Users.GetByID(1)
+	if err != nil {
+		t.Error("Something went wrong, unable to get user: ", err)
+	}
+
+	if u.LastName != "Kiriakou" {
+		t.Error("Something went wrong, unable to update user last name.")
 	}
 }
