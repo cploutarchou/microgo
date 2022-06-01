64984f3fd23a336f7aa7643a2acef07e85c1b2a7
Christos <cploutarchou@gmail.com>
2022-06-01 21:41:04 +0300

    Finalize validation package

--- microGo/validator.go
+++ microGo/validator.go
@@ -10,25 +10,25 @@ import (
 )
 
 type Validation struct {
-	Data  url.Values
-	Error map[string]string
+	Data   url.Values
+	Errors map[string]string
 }
 
 func (m *MicroGo) Validator(data url.Values) *Validation {
 	return &Validation{
-		Error: make(map[string]string),
-		Data:  data,
+		Errors: make(map[string]string),
+		Data:   data,
 	}
 
 }
 
 func (v *Validation) Valid() bool {
-	return len(v.Error) == 0
+	return len(v.Errors) == 0
 }
 
 func (v *Validation) AddError(key, message string) {
-	if _, exists := v.Error[key]; !exists {
-		v.Error[key] = message
+	if _, exists := v.Errors[key]; !exists {
+		v.Errors[key] = message
 	}
 }
 
