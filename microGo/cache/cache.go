1de64d2166a503bb3ad7e6925cedb7fb4c54f0f2
Christos <cploutarchou@gmail.com>
2022-06-05 18:20:29 +0300

    Implement Set/Get/Delete/DeleteIfMatch/Empty function for redis cache interface

--- microGo/cache/cache.go
+++ microGo/cache/cache.go
@@ -1,6 +1,8 @@
 package cache
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
 	"github.com/gomodule/redigo/redis"
 )
@@ -10,7 +12,7 @@ type Cache interface {
 	Get(string) (interface{}, error)
 	Set(string, interface{}, ...int) error
 	Delete(string) error
-	EmptyIfMatch(string)
+	DeleteIfMatch(string) error
 	Empty() error
 }
 
@@ -21,6 +23,7 @@ type RedisCache struct {
 
 type Entry map[string]interface{}
 
+//Exist :  Check if the key exists
 func (c *RedisCache) Exist(key string) (bool, error) {
 	_key := fmt.Sprintf("%s:%s", c.Prefix, key)
 
@@ -32,27 +35,142 @@ func (c *RedisCache) Exist(key string) (bool, error) {
 	}
 	return ok, nil
 }
-func (c *RedisCache) Get(s string) (interface{}, error) {
-	//TODO implement me
-	panic("implement me")
+
+//encode : Encode a string/s  of type entry
+func encode(item Entry) ([]byte, error) {
+	b := bytes.Buffer{}
+	e := gob.NewEncoder(&b)
+	err := e.Encode(item)
+	if err != nil {
+		return nil, err
+	}
+	return b.Bytes(), nil
 }
 
-func (c *RedisCache) Set(s string, i interface{}, i2 ...int) error {
-	//TODO implement me
-	panic("implement me")
+//decode : Decode  a string/s  of type entry
+func decode(str string) (Entry, error) {
+	item := Entry{}
+	b := bytes.Buffer{}
+	b.Write([]byte(str))
+	d := gob.NewDecoder(&b)
+	err := d.Decode(&item)
+	if err != nil {
+		return nil, err
+	}
+	return item, nil
 }
 
-func (c *RedisCache) Delete(s string) error {
-	//TODO implement me
-	panic("implement me")
+//Get : Return Key values from Redis if it exists.
+func (c *RedisCache) Get(key string) (interface{}, error) {
+	_key := fmt.Sprintf("%s:%s", c.Prefix, key)
+	conn := c.Connect.Get()
+	defer conn.Close()
+	cacheEntry, err := redis.Bytes(conn.Do("GET", _key))
+	if err != nil {
+		return nil, err
+	}
+	decoded, err := decode(string(cacheEntry))
+	if err != nil {
+		return nil, err
+	}
+	item := decoded[key]
+	return item, nil
 }
 
-func (c *RedisCache) EmptyIfMatch(s string) {
-	//TODO implement me
-	panic("implement me")
+//Set : Set a key value in Redis
+func (c *RedisCache) Set(key string, value interface{}, expires ...int) error {
+	_key := fmt.Sprintf("%s:%s", c.Prefix, key)
+	conn := c.Connect.Get()
+	defer conn.Close()
+
+	entry := Entry{}
+	entry[_key] = value
+	encoded, err := encode(entry)
+	if err != nil {
+		return err
+	}
+
+	if len(expires) > 0 {
+		_, err = conn.Do("SEMTEX", _key, expires[0], string(encoded))
+		if err != nil {
+			return err
+		}
+	} else {
+		_, err = conn.Do("SET", _key, string(encoded))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
+//Delete : Delete a key value in Redis.
+func (c *RedisCache) Delete(key string) error {
+	_key := fmt.Sprintf("%s:%s", c.Prefix, key)
+	conn := c.Connect.Get()
+	defer conn.Close()
+	_, err := conn.Do("DELETE", _key)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//DeleteIfMatch : Delete a key values where match the with the key value
+func (c *RedisCache) DeleteIfMatch(key string) error {
+	_key := fmt.Sprintf("%s:%s", c.Prefix, key)
+	keys, err := c.getKeys(_key)
+	if err != nil {
+		return err
+	}
+	for _, i := range keys {
+		err := c.Delete(i)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+//Empty : Delete all entries from redis.
 func (c *RedisCache) Empty() error {
-	//TODO implement me
-	panic("implement me")
+	_keys := fmt.Sprintf("%s:", c.Prefix)
+	conn := c.Connect.Get()
+	defer conn.Close()
+	keys, err := c.getKeys(_keys)
+	if err != nil {
+		return err
+	}
+	for _, i := range keys {
+		err := c.Delete(i)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+//getKeys : Return all keys that match to the pattern.
+func (c *RedisCache) getKeys(pattern string) ([]string, error) {
+	conn := c.Connect.Get()
+	defer conn.Close()
+	iter := 0
+	var keys []string
+	for {
+		arr, err := redis.Values(conn.Do("SCAN", iter, "MATCH", fmt.Sprintf("%s*", pattern)))
+		if err != nil {
+			return keys, err
+		}
+		iter, _ = redis.Int(arr[0], nil)
+		k, _ := redis.Strings(arr[1], nil)
+		keys = append(keys, k...)
+
+		if iter == 0 {
+			break
+		}
+
+	}
+
+	return keys, nil
 }
