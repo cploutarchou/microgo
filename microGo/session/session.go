1df04a33a6b533a2b7d873475f3c4f8341111992
Christos <cploutarchou@gmail.com>
2022-06-07 21:40:19 +0300

    Add redis store session to microgo Package

--- microGo/session/session.go
+++ microGo/session/session.go
@@ -1,34 +1,12 @@
-/*
- * MIT License
- *
- * Copyright (c) 2022 Christos Ploutarchou
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 package session
 
 import (
 	"database/sql"
 	"github.com/alexedwards/scs/mysqlstore"
 	"github.com/alexedwards/scs/postgresstore"
+	"github.com/alexedwards/scs/redisstore"
 	"github.com/alexedwards/scs/v2"
+	"github.com/gomodule/redigo/redis"
 	"net/http"
 	"strconv"
 	"strings"
@@ -43,6 +21,7 @@ type Session struct {
 	SessionType    string
 	IsCookieSecure string
 	DBPool         *sql.DB
+	RedisPool      *redis.Pool
 }
 
 func (s *Session) InitializeSession() *scs.SessionManager {
@@ -77,6 +56,7 @@ func (s *Session) InitializeSession() *scs.SessionManager {
 
 	switch strings.ToLower(s.SessionType) {
 	case "redis":
+		session.Store = redisstore.New(s.RedisPool)
 	case "mysql", "mariadb":
 		session.Store = mysqlstore.New(s.DBPool)
 	case "postgres", "postgresql":
