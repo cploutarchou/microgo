30f403270f9d4e809c862a2ff7b845eba2ed4c50
Christos Ploutarchou <cploutarchou@gmail.com>
2023-02-01 00:06:27 +0200

    Modify install script add support for ARM64 cpu

--- install.sh
+++ install.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
+
 userDir=$USER
 echo "Installing MicroGO binaries..."
-curl -LJO https://github.com/cploutarchou/MicroGO/releases/download/v1.0.5/microGo
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    if grep -q "alias microGo" ~/.zprofile; then
-        echo "Already exported"
-        sudo mv microGo /usr/local/bin/microGo
-        chmod +x /usr/local/bin/microGo
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    if [[ $(uname -m) == "x86_64" ]]; then
+        curl -LJO https://github.com/cploutarchou/MicroGO/releases/download/v1.0.5/microGo
+        mv microGo /Users/"$userDir"/go/bin/microGo
     else
-        sudo mv microGo /usr/local/bin/microGo
-        chmod +x /usr/local/bin/microGo
-        echo "export PATH=$PATH:/usr/local/bin/microGo" >>/home/"$userDir"/.bashrc
-        echo "MicroGO binaries exported to PATH"
-        source /home/"$userDir"/.bashrc
+        curl -LJO https://github.com/cploutarchou/MicroGO/releases/download/v1.0.5/microGo-MacOS-ARM64
+        mv microGo-MacOS-ARM64 /Users/"$userDir"/go/bin/microGo
     fi
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    mv microGo /Users/"$userDir"/go/bin/microGo
+    
     echo "Enter your password to install MicroGO binaries using sudo"
-    chmod +x /Users/"$userDir"/go/bin/microGo # move to /usr/local/bin/microGo
+    chmod +x /Users/"$userDir"/go/bin/microGo
     echo "MicroGO binaries installed"
     echo "Export env"
+    
     if grep -q "alias microGo" ~/.zprofile; then
         echo "Already exported"
     else
         source ~/.zprofile
         comm="alias microGo=/Users/$userDir/go/bin/microGo"
-        echo $comm >>/Users/"$userDir"/.zprofile # add to .zprofile
+        echo $comm >>/Users/"$userDir"/.zprofile
         echo "MicroGO binaries exported to PATH"
         source ~/.zprofile
     fi
