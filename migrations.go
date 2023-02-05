487ec3ef0bba8e4b8202bbb8f6263bbbf10087a6
Christos Ploutarchou <cploutarchou@gmail.com>
2023-02-05 22:46:32 +0200

    Refactor

--- migrations.go
+++ migrations.go
@@ -6,9 +6,8 @@ import (
 	"runtime"
 	"strings"
 
-	"github.com/golang-migrate/migrate/v4"
-
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/mysql"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
