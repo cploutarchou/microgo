96bb148310ba9ec76600d7300ca2b939b921ecca
Christos Ploutarchou <cploutarchou@gmail.com>
2023-02-06 23:14:45 +0200

    create admin tables migration for mysql and mariadb

---
+++ terminal/cli/templates/migrations/admin_tables.mysql.sql
@@ -0,0 +1,46 @@
+DROP TABLE IF EXISTS `role_users`,
+`permission_roles` CASCADE;
+DROP TABLE IF EXISTS `roles`,
+`permissions`;
+CREATE TABLE `roles` (
+    `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `description` VARCHAR(255) NOT NULL,
+    `created_at` TIMESTAMP NULL DEFAULT NULL,
+    `updated_at` TIMESTAMP NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `roles_name_unique` (`name`)
+) ENGINE = InnoDB AUTO_INCREMENT = 17 DEFAULT CHARSET = utf8mb4;
+CREATE TABLE `role_users` (
+    `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `user_id` INT(10) UNSIGNED NOT NULL,
+    `role_id` INT(10) UNSIGNED NOT NULL,
+    `created_at` TIMESTAMP NULL DEFAULT NULL,
+    `updated_at` TIMESTAMP NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `role_users_user_id_foreign` (`user_id`),
+    KEY `role_users_role_id_foreign` (`role_id`),
+    CONSTRAINT `role_users_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `role_users_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 17 DEFAULT CHARSET = utf8mb4;
+CREATE TABLE `permissions` (
+    `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `description` VARCHAR(255) NOT NULL,
+    `created_at` TIMESTAMP NULL DEFAULT NULL,
+    `updated_at` TIMESTAMP NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `permissions_name_unique` (`name`)
+) ENGINE = InnoDB AUTO_INCREMENT = 17 DEFAULT CHARSET = utf8mb4;
+CREATE TABLE permission_roles (
+    id int(10) unsigned NOT NULL AUTO_INCREMENT,
+    permission_id int(10) unsigned NOT NULL,
+    role_id int(10) unsigned NOT NULL,
+    created_at timestamp NULL DEFAULT NULL,
+    updated_at timestamp NULL DEFAULT NULL,
+    PRIMARY KEY (id),
+    KEY permission_roles_permission_id_foreign (permission_id),
+    KEY permission_roles_role_id_foreign (role_id),
+    CONSTRAINT permission_roles_permission_id_foreign FOREIGN KEY (permission_id) REFERENCES permissions (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT permission_roles_role_id_foreign FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 17 DEFAULT CHARSET = utf8mb4;
