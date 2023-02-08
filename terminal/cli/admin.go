6a902676e2eb9b40a9861a7fd881a300f4a664f9
Christos Ploutarchou <cploutarchou@gmail.com>
2023-02-08 21:37:16 +0200

    Extend createAdminUser functionality

--- terminal/cli/admin.go
+++ terminal/cli/admin.go
@@ -2,9 +2,39 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func createAdminUser() error {
+	askForInput("")
+	dbType := micro.DB.DatabaseType
+	
+	if dbType == "mariadb" {
+		dbType = "mysql"
+	}
+
+	if dbType == "postgresql" {
+		dbType = "postgres"
+	}
+	fileName := fmt.Sprintf("%d_create_auth_tables", time.Now().UnixMicro())
+	upFile := micro.RootPath + "/migrations/" + fileName + ".up.sql"
+	downFile := micro.RootPath + "/migrations/" + fileName + ".down.sql"
+
+	err := copyTemplateFile("templates/migrations/admin_tables."+dbType+".sql", upFile)
+	if err != nil {
+		gracefullyExit(err)
+	}
+
+	err = copyDataToFile([]byte("drop table if exists users cascade; drop table if exists tokens cascade; drop table if exists remember_tokens;"), downFile)
+	if err != nil {
+		gracefullyExit(err)
+	}
+
+	// run migrations
+	err = doMigrate("up", "")
+	if err != nil {
+		gracefullyExit(err)
+	}
 	// ask for email
 	email, err := askForInput("Enter admin email: ")
 	if err != nil {
@@ -26,88 +56,13 @@ func createAdminUser() error {
 	}
 	// create user
 
-	dbType := micro.DB.DatabaseType
-
-	if dbType == "mariadb" {
-		dbType = "mysql"
-	}
-
-	if dbType == "postgresql" {
-		dbType = "postgres"
-	}
-
-	// create the required tables in the database for roles , permissions and users if they don't exist
-
-	_, err = micro.DB.Pool.Exec(`CREATE TABLE IF NOT EXISTS users (
-		id SERIAL,
-		email VARCHAR(255) NOT NULL,
-		password VARCHAR(255) NOT NULL,
-		created_at TIMESTAMP NOT NULL,
-		updated_at TIMESTAMP NOT NULL,
-		deleted_at TIMESTAMP NULL,
-		CONSTRAINT users_pkey PRIMARY KEY (id)
-	);`)
+	fmt.Println("Creating admin user...")
+	_, err = micro.DB.Pool.Exec("INSERT INTO users (email, password, created_at, updated_at) VALUES ($1, $2, $3, $4)", email, password, time.Now(), time.Now())
 	if err != nil {
 		return err
 	}
-
-	_, err = micro.DB.Pool.Exec(`CREATE TABLE IF NOT EXISTS roles (
-		id SERIAL,
-		name VARCHAR(255) NOT NULL,
-		created_at TIMESTAMP NOT NULL,
-		updated_at TIMESTAMP NOT NULL,
-		deleted_at TIMESTAMP NULL,
-		CONSTRAINT roles_pkey PRIMARY KEY (id)
-	);`)
-	if err != nil {
-		return err
-	}
-
-	_, err = micro.DB.Pool.Exec(`CREATE TABLE IF NOT EXISTS permissions (
-		id SERIAL,
-		name VARCHAR(255) NOT NULL,
-		created_at TIMESTAMP NOT NULL,
-		updated_at TIMESTAMP NOT NULL,
-		deleted_at TIMESTAMP NULL,
-		CONSTRAINT permissions_pkey PRIMARY KEY (id)
-	);`)
-	if err != nil {
-		return err
-	}
-
-	_, err = micro.DB.Pool.Exec(`CREATE TABLE IF NOT EXISTS role_permissions (
-		id SERIAL,
-		role_id INT NOT NULL,
-		permission_id INT NOT NULL,
-		created_at TIMESTAMP NOT NULL,
-		updated_at TIMESTAMP NOT NULL,
-		deleted_at TIMESTAMP NULL,
-		CONSTRAINT role_permissions_pkey PRIMARY KEY (id)
-	);`)
-	if err != nil {
-		return err
-	}
-
-	_, err = micro.DB.Pool.Exec(`CREATE TABLE IF NOT EXISTS user_roles (
-		id SERIAL,
-		user_id INT NOT NULL,
-		role_id INT NOT NULL,
-		created_at TIMESTAMP NOT NULL,
-		updated_at TIMESTAMP NOT NULL,
-		deleted_at TIMESTAMP NULL,
-		CONSTRAINT user_roles_pkey PRIMARY KEY (id)
-	);`)
-	if err != nil {
-		return err
-	}
-
-	// insert the default roles if they don't exist
-	_, err = micro.DB.Pool.Exec(`INSERT INTO roles (name, created_at, updated_at) VALUES ('admin', NOW(), NOW()) ON CONFLICT (name) DO NOTHING;`)
-	if err != nil {
-		return err
-	}
-
-	
+	fmt.Println("Admin user created successfully!")
+	return nil
 
 }
 
